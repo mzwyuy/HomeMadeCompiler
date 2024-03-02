d4eaf4d8633992bbc05842671f3b5aa17c144da5
Yanshi <2571030311@qq.com>
2024-03-02 17:26:03 +0800

    Add more unit test cases and include 3rd lib of gtest

--- test_cases/test_main.cpp
+++ test_cases/test_main.cpp
@@ -4,43 +4,56 @@
 class MyTest : public testing::Test {
 protected:
     // 在每个测试用例执行前设置测试环境
-    void SetUp() override {
-        // 在这里执行一些准备工作，比如初始化资源
-        // 这里我们简单地输出一条消息表示设置完成
-        std::cout << "Setting up test environment..." << std::endl;
-    }
+    void SetUp() override {}
 
     // 在每个测试用例执行后清理测试环境
-    void TearDown() override {
-        // 在这里执行一些清理工作，比如释放资源
-        // 这里我们简单地输出一条消息表示清理完成
-        std::cout << "Tearing down test environment..." << std::endl;
-    }
+    void TearDown() override {}
 };
 
-// 测试用例
-TEST_F(MyTest, Test1) {
-    std::cout << "Test1 passed!" << std::endl;
-
+void ParseAndTestFile(const char *file_name) {
     ParseIR ir_parser;
-    std::string filename = "D:\\cpp\\cpp_code\\tiny_compiler\\test_cases\\test.txt";
-    FILE* c_file = fopen(filename.c_str(), "r");
+    char path[255];
+    getcwd(path, 255);
+    std::string absolute_file_name = path;
+    absolute_file_name.erase(absolute_file_name.size() - 3);   // remove bin
+    absolute_file_name += "test_cases\\";  // .../bin/../test_cases
+    absolute_file_name += file_name;
+
+    FILE *c_file = fopen(absolute_file_name.c_str(), "r");
     if (c_file) {
         std::cout << "open file successfully" << std::endl;
-        ir_parser.DoLexemeInterprete(c_file, filename);
+        ir_parser.DoLexemeInterprete(c_file, absolute_file_name);
         auto gloabl_scope = ir_parser.SyntaxInpterprete();
         gloabl_scope->Display(std::cout, 0);
         fclose(c_file);
     } else {
-        std::cout << "can't open file" << std::endl;
+        std::cout << "can't open file:" << std::endl;
+        std::cout << absolute_file_name << std::endl;
     }
 }
 
-TEST_F(MyTest, Test2) {
-// 在这里编写另一个测试代码
-// 这里我们简单地输出一条消息表示测试通过
-std::cout << "Test2 passed!" << std::endl;
+// 测试用例
+TEST_F(MyTest, Test1) {
+    ParseAndTestFile("simple_test0.txt");
 }
+
+TEST_F(MyTest, RomanToInt) {
+    ParseAndTestFile("RomanToInt.txt");
+}
+
+TEST_F(MyTest, ReverseInt) {
+    ParseAndTestFile("ReverseInt.txt");
+}
+
+TEST_F(MyTest, BitwiseCalculation) {
+    ParseAndTestFile("BitwiseCalculation.txt");
+}
+
+TEST_F(MyTest, Test5) {
+    // ParseAndTestFile("ReverseList.txt");
+    // todo: need to support class
+}
+
 
 int main(int argc, char** argv) {
     // 初始化 Google Test 框架
